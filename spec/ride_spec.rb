c56aa46ee538313e980449f47cc69f2a1bd79728
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-10 10:21:12 -0500

    Added tests to check if i can create the ride class

---
+++ spec/ride_spec.rb
@@ -0,0 +1,17 @@
+require './lib/ride'
+RSpec.describe Ride do
+  before(:each) do
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills}) 
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+  end
+  it "exists and has attr" do
+    expect(@ride1).to be_a(Ride)
+    expect(@ride1.name).to eq("Walnut Creek Trail")
+    expect(@ride1.distance).to eq(10.7)
+    expect(@ride1.terrain).to eq(:hills)
+    expect(@ride1.loop?).to eq(false)
+  end
+  # it 'calculates total distance' do
+  #   expect(@ride1.total_distance).to e
+  # end
+end
