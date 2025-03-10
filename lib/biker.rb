eca1e407e84711404d9431132baf46a17c48913e
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-10 12:28:07 -0500

    modified the tests and methods log_ride and personal_record

--- lib/biker.rb
+++ lib/biker.rb
@@ -11,17 +11,22 @@ class Biker
     @acceptable_terrain << terrain
   end
 
-  def log_ride(ride, times)
-    if @rides[ride]
-      @rides[ride] << times
-    else
-      @rides[ride] = [times]
+  def log_ride(ride, times)#bikers can log a ride with a time
+    if @acceptable_terrain.include?(ride.terrain) && ride.total_distance <= @max_distance
+      if @rides[ride]
+        @rides[ride] << times
+      else
+        @rides[ride] = [times]
+      end
     end
     @rides
   end
 
   def personal_record(ride)
-    @rides[ride].min
-    
+    if @rides [ride]
+      @rides[ride].min
+    else
+      false
+    end
   end
 end
