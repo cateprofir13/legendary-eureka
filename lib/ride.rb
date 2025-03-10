672ff1417c7e862334224b5ace86901ba5f2ba6e
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-10 10:21:41 -0500

    Initialiazed the ride class

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,12 @@
 class Ride
-
+  attr_reader :name, :distance, :terrain
+  def initialize(data)
+    @name = data[:name]
+    @distance = data[:distance]
+    @terrain = data[:terrain]
+    @loop = data[:loop] = false
+  end
+  def loop?
+    @loop
+  end
 end
