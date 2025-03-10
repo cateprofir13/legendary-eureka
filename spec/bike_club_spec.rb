3a194eed96efb096aff81536fe86bca4a6636658
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-10 12:48:23 -0500

    tested and defined the most_rides method

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -5,10 +5,40 @@ require './lib/bike_club'
 RSpec.describe BikeClub do
   before(:each) do
     @bike_club = BikeClub.new("Bikers Unite")
+    @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+    @biker3 = Biker.new("Cate", 100)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
 
   it "exists and has a name" do
     expect(@bike_club).to be_a(BikeClub)
     expect(@bike_club.name).to eq("Bikers Unite")
+    expect(@bike_club.bikers).to eq([])
+  end
+
+  it "returns an array of biker" do
+    @bike_club.add_biker(@biker1)
+    @bike_club.add_biker(@biker2)
+    @bike_club.add_biker(@biker3)
+
+    expect(@bike_club.bikers).to eq([@biker1, @biker2, @biker3])
+
+  end
+
+  it "returns the biker who took most_rides" do
+
+    @biker1.log_ride(@ride1, 92.5)
+    @biker1.log_ride(@ride1, 91.1)
+    @biker2.log_ride(@ride2, 60.9)
+    @biker3.log_ride(@ride2, 61.6)
+
+    @bike_club.add_biker(@biker1)
+    @bike_club.add_biker(@biker2)
+    @bike_club.add_biker(@biker3)
+
+    expect(@bike_club.most_rides).to eq()
+
   end
 end
