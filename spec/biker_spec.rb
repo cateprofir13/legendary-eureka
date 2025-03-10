1e4224bbc4129c9e21c8ad344074b2266121b5ba
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-10 12:27:29 -0500

    modified the log_ride method

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -3,6 +3,7 @@ require './lib/biker'
 RSpec.describe Biker do
   before(:each) do
     @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
 
@@ -18,11 +19,20 @@ RSpec.describe Biker do
   it 'returns an arrya of acceptable_terrain' do
     @biker.learn_terrain(:gravel)
     @biker.learn_terrain(:hills)
+    @biker2.learn_terrain(:gravel)
+    @biker2.learn_terrain(:hills)
 
     expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
+    expect(@biker2.acceptable_terrain).to eq([:gravel, :hills])
   end
 
-  it "can log rides" do
+  it 'logs rides' do
+
+    @biker.learn_terrain(:gravel)
+    @biker.learn_terrain(:hills)
+    @biker2.learn_terrain(:gravel)
+    @biker2.learn_terrain(:hills)
+
     @biker.log_ride(@ride1, 92.5)
     @biker.log_ride(@ride1, 91.1)
     @biker.log_ride(@ride2, 60.9)
@@ -32,16 +42,31 @@ RSpec.describe Biker do
            @ride2 => [60.9, 61.6]
          }
     expect(@biker.rides).to eq(expected)
+
+    @biker2.log_ride(@ride1, 95.0)
+    @biker2.log_ride(@ride2, 65.0)
+
+    expect(@biker2.rides).to eq({@ride2 => [65]})
+
   end
 
-  it 'returns the personal record ' do
+  it 'returns the personal record' do
+
+    @biker.learn_terrain(:gravel)
+    @biker.learn_terrain(:hills)
+    @biker2.learn_terrain(:gravel)
+    @biker2.learn_terrain(:hills)
+
     @biker.log_ride(@ride1, 92.5)
     @biker.log_ride(@ride1, 91.1)
     @biker.log_ride(@ride2, 60.9)
     @biker.log_ride(@ride2, 61.6)
+    @biker2.log_ride(@ride2, 65.0)
 
     expect(@biker.personal_record(@ride1)).to eq(91.1)
-    expect(@biker.personal_record(@ride2)).to eq(60.9)
+    expect(@biker2.personal_record(@ride2)).to eq(65.0)
 
   end
+  
+  
 end
