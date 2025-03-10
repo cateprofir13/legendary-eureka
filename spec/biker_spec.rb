65b0d00f7df84aefe2d32d17fd7280e4498eab03
Ecaterina Profir <ecaterina.profir@gmail.com>
2025-03-10 11:05:58 -0500

    test the log_rides method

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -21,4 +21,16 @@ RSpec.describe Biker do
 
     expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
   end
+
+  it "can log rides" do
+    @biker.log_ride(@ride1, 92.5)
+    @biker.log_ride(@ride1, 91.1)
+    @biker.log_ride(@ride2, 60.9)
+    @biker.log_ride(@ride2, 61.6)
+    expected = {
+           @ride1 => [92.5, 91.1],
+           @ride2 => [60.9, 61.6]
+         }
+    expect(@biker.rides).to eq(expected)
+  end
 end
